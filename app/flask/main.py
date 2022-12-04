8dd3bebb51516f75b229094c34e4335c5cc89ae1
bigface0202 <theshoreunder@gmail.com>
2022-12-04 12:04:57 +0900

    Create flask

---
+++ app/flask/main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template
+
+# Vueでビルド先のディレクトリを指定
+app = Flask(__name__, static_folder='../vue/dist/static', template_folder='../vue/dist/templates')
+
+# SPA対応
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
