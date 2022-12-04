044d0b73997fd7fb5f672d647700346f3c5bf7cd
bigface0202 <theshoreunder@gmail.com>
2022-12-04 16:24:39 +0900

    Change delimiter of vuejs

--- app/vue/vue.config.js
+++ app/vue/vue.config.js
@@ -4,4 +4,17 @@ module.exports = defineConfig({
   outputDir: 'dist',
   assetsDir: 'static',
   indexPath: 'templates/index.html',
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.compilerOptions = { 
+          whitespace: 'condense',
+          delimiters: ['[[', ']]']
+        };
+        return options;
+      });
+  }
 })
